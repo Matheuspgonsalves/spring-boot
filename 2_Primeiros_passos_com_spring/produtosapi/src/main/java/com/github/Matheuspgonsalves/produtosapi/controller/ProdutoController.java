8591c99adc202822daf6136a1ff733a512e56f37
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-11-14 12:29:01 -0300

    Criando busca personalizada

--- 2_Primeiros_passos_com_spring/produtosapi/src/main/java/com/github/Matheuspgonsalves/produtosapi/controller/ProdutoController.java
+++ 2_Primeiros_passos_com_spring/produtosapi/src/main/java/com/github/Matheuspgonsalves/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.github.Matheuspgonsalves.produtosapi.model.Produto;
 import com.github.Matheuspgonsalves.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -46,5 +47,10 @@ public class ProdutoController {
                           @RequestBody Produto produto){
         produto.setId(id);
         produtoRepository.save(produto);
+    }
+
+    @GetMapping("")
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
     }
 }
